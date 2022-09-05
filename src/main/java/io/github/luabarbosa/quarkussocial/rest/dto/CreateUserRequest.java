5cea727a4226f553c7c35d0d779238eed32e30a7
luabarbosa <lua.barbosa@yahoo.com.br>
2022-09-05 15:57:18 -0300

    Feat: Criando dto e resource

---
+++ src/main/java/io/github/luabarbosa/quarkussocial/rest/dto/CreateUserRequest.java
@@ -0,0 +1,22 @@
+package io.github.luabarbosa.quarkussocial.rest.dto;
+
+public class CreateUserRequest {
+    String name;
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
